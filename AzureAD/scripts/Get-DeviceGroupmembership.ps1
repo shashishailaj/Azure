7b351f4f8429c0315a2ebc30dbd8e444c756c501
ShashiShailaj-MSFT <532022+shashishailaj@users.noreply.github.com>
2021-10-21 15:42:30 +0530

    Add details and credit .
    Updates. This can be optimized further as per changes in the Azure AD at the moment and the graph API .

--- AzureAD/scripts/Get-DeviceGroupmembership.ps1
+++ AzureAD/scripts/Get-DeviceGroupmembership.ps1
@@ -22,5 +22,37 @@ Using Microsoft Grpah Module
 
 Install-Module -Name Microsoft.Graph
 Connect-Graph -Scopes "User.Read.All", "Group.ReadWrite.All", "Device.Read.All"
-$DeviceGroupData = Invoke-GraphRequest -Uri "https://graph.microsoft.com/v1.0/devices/<deviceid>/memberOf"
+$DeviceGroupMembership = Invoke-GraphRequest -Uri "https://graph.microsoft.com/v1.0/devices/<deviceid>/memberOf"
 $DeviceGroupData.value.displayName
+
+
+Credits - https://stackoverflow.com/questions/57251857/azure-active-directory-how-to-check-device-membership
+
+Get-AzureADGroup -All 1 | ? {"COMPUTER_DISPLAY_NAME" -in (Get-AzureADGroupMember -ObjectId $_.ObjectId).DisplayName} -Filter groupTypes/any(c:c+ne+'Unified')
+
+function Get-AzureADDeviceMembership{
+    [CmdletBinding()]
+    Param(
+        [string]$ComputerDisplayname,
+        [switch]$UseCache
+    )
+    if(-not $Global:AzureAdGroupsWithMembers -or -not $UseCache){
+        write-host "refreshing cache"
+        $Global:AzureAdGroupsWithMembers = Get-AzureADGroup -All 1 | % {
+            $members = Get-AzureADGroupMember -ObjectId $_.ObjectId
+            $_ | Add-Member -MemberType NoteProperty -Name Members -Value $members
+            $_
+        }
+    }
+    $Global:AzureAdGroupsWithMembers | % {
+        if($ComputerDisplayname -in ($_.Members | select -ExpandProperty DisplayName)){
+            $_
+        }
+    } | select -Unique
+}
+
+Connect-AzureAD    
+Get-AzureADDeviceMembership -ComputerDisplayname "COMPUTER_DISPLAY_NAME" -UseCache
+
+
+-Filter groupTypes/any(c:c+ne+'Unified')
